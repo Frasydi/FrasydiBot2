a0fb89f77fc6f606e18cde32b8b719df3529242a
Frasydi <frasydi137@gmail.com>
2024-07-25 16:10:27 +0700

    fix

--- controller/unpermission.ts
+++ controller/unpermission.ts
@@ -2,11 +2,11 @@ import { WASocket } from '@whiskeysockets/baileys';
 import ControllerFunctions, { kategoris } from '../controller_add';
 import { messageType } from '../controller_middleware';
 import { getOptions } from '../util/option';
-export const types = /permission/i
-export const nama = "Permission"
+export const types = /unpermission/i
+export const nama = "Unpermission"
 export const kategori = "Owner"
 export const bantuan = [
-    getOptions()?.prefix+"permission [mention]"
+    getOptions()?.prefix+"unpermission [mention]"
 ]
 export const isGroup = false
 export const isAdmin = false
